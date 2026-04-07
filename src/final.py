90eab9bdee88ebf472876ee876e18e3bdce912eb
dippypixel <dapixel8ter@gmail.com>
2026-04-07 15:50:27 -0500

    added circular striker than can be moved around with mouse

--- src/final.py
+++ src/final.py
@@ -1,24 +1,49 @@
 import pygame
 
+# CONSTANTS
+
+SCRNWIDTH = 1920
+SCRNHEIGHT = 1080
+
+class Striker():
+
+    def __init__(self, pos=(0,0), size=30):
+        print("Striker Exist")
+        self.pos = pos
+        self.size = size
+        self.color = pygame.Color(0,255,0)
+        self.surface = pygame.Surface((self.size,self.size),pygame.SRCALPHA)
+        self.shape = pygame.Rect(0, 0, self.size, self.size)
+    def update(self,surface):
+        x,y = pygame.mouse.get_pos()
+        y = max(y,SCRNHEIGHT//2)
+        self.pos = (x,y)
+        self.draw(surface)
+    def draw(self,surface):
+        pygame.draw.ellipse(self.surface, self.color, self.shape)
+        surface.blit(self.surface,self.pos)
 def main():
 
     pygame.init()
     pygame.display.set_caption("Breakout:Force")
+    pygame.mouse.set_visible(False)
     clock= pygame.time.Clock()
-    dt = 0
-    resolution =(1920,1080)
+    #values
+    dt = 0.0
+    resolution =(SCRNWIDTH,SCRNHEIGHT)
+    black = pygame.Color(0,0,0)
     screen= pygame.display.set_mode(resolution)
+    striker = Striker()
     running = True
     while running:
+        #QUIT DETECTION
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False      
-        #GAME LOGIC
-        #Render and Display
-        black = pygame.Color(0,0,0)
+                running = False   
         screen.fill(black)
+        striker.update(screen)
         pygame.display.flip()
-        dt = clock.tick(12)
+        dt = clock.tick(60)
     pygame.quit()
 
 
