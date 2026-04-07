331e8a8565f336ef9311aede7bd90dff95192413
dippypixel <dapixel8ter@gmail.com>
2026-04-07 17:30:15 -0500

    added simple collision system

--- src/final.py
+++ src/final.py
@@ -1,61 +1,89 @@
 import pygame
-
+#------------------------------------------------------------------------
 # CONSTANTS
 
-SCRNWIDTH = 1920
+SCRNWIDTH = 720
 SCRNHEIGHT = 1080
-
+#------------------------------------------------------------------------
 class Striker():
 
-    def __init__(self, pos=(0,0), radius=30):
-        print("Striker Exist")
+    def __init__(self, pos=(0,0), radius=60):
         self.pos = pos
         self.radius = radius
         self.color = pygame.Color(0,255,0)
-        self.surface = pygame.Surface((self.radius,self.radius),
-                                                pygame.SRCALPHA)
         self._rect = pygame.Rect(0, 0, self.radius, self.radius)
     def update(self,surface):
         x,y = pygame.mouse.get_pos()
         x =  min(x,SCRNWIDTH-self.radius)
         y = max(min(y,SCRNHEIGHT-self.radius),SCRNHEIGHT//2)
-        self._rect.update(x,y,self.radius, self.radius)
-        self.draw(surface)
+
+        self.vel_x = x - self._rect.x
+        self.vel_y = y - self._rect.y
+
+        self._rect.update(x,y,self._rect.width, self._rect.height)
+        self.draw(surface)  
+
+
     def draw(self,surface):
         pygame.draw.rect(surface, self.color, self._rect)
 
-class Ball(pygame.sprite.Sprite):
+#------------------------------------------------------------------------
+class Ball():
 
     def __init__(self, pos=(0,0), radius=60):
-        print("Ball Exist")
         self.pos = pos
         self.radius = radius
         self.color = pygame.Color(255,255,255)
-        self.surface = pygame.Surface((self.radius,self.radius),
-                                                pygame.SRCALPHA)
         self._rect = pygame.Rect(0, 0, self.radius, self.radius)
-
+        self.vel_rect = pygame.Rect(0, 0, self.radius, self.radius)
         self.pos_x,self.pos_y = self.pos
+        self.vel_x = 0.0
         self.vel_y = 0.0
+        self._hit = False
+        self._wallhit = False
 
     def update(self,surface,dt):
-        self.apply_gravity(dt)
-        self._rect.update(self.pos_x,self.pos_y,self.radius, self.radius)
-        self.draw(surface)
-
-    def apply_gravity(self,dt):
+        #Update Position
         self.vel_y += (dt*0.01)
-        self.pos_y += self.vel_y   
+        self.pos_x += self.vel_x
+        self.pos_y += self.vel_y
 
-    def bounce(self,striker):
-        if self._rect.colliderect(striker._rect):
-            print("True")
-            self.vel_y *= -.9   
+        self._rect.update(self.pos_x,self.pos_y,self.radius, self.radius)
+        self.draw(surface) 
+
+    def ball_striker_collision(self,striker):
+        self.vel_rect = pygame.Rect(self.pos_x-15, 
+                               self.pos_y + self.vel_y*2, 
+                               self.radius*1.5, self.radius)
+        if self.vel_rect.colliderect(striker._rect):
+            if self._hit == False:
+                if abs(striker.vel_y) > 10:
+                    self.vel_x = striker.vel_x
+                else:
+                    self.vel_x = (self._rect.x - striker._rect.x)//5
+                if striker.vel_y < -10:
+                    self.vel_y = striker.vel_y
+                else:
+                    self.vel_y *= -.7
+                self._hit = True
+        else:
+            self._hit = False
+    def wall_collision(self):
+        if self._wallhit == False:
+            if self._rect.y <= 0 + self.radius and self.vel_y < 0:
+                self.vel_y *= -.7
+            if (self._rect.x <= 0 + self.radius-self.vel_x 
+                or self._rect.x+self.vel_x  >= SCRNWIDTH - self.radius):
+                self.vel_x *= -.7
+            self._wallhit = True
+        else:
+            self._wallhit = False
 
     def draw(self,surface):
+       # pygame.draw.rect(surface, pygame.Color(144,144,144), self.vel_rect)
         pygame.draw.rect(surface, self.color, self._rect)
 
-        
+#------------------------------------------------------------------------        
 def main():
 
     pygame.init()
@@ -81,10 +109,10 @@ def main():
         screen.fill(black)      
         striker.update(screen)
         ball.update(screen,dt)    
-        ball.bounce(striker)   
+        ball.ball_striker_collision(striker)   
+        ball.wall_collision()
         pygame.display.flip()
         dt = clock.tick(60)
-        print(dt)
     pygame.quit()
 
 
