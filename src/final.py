c89edb3e481ceee26edef6286edc734b83e23843
dippypixel <dapixel8ter@gmail.com>
2026-04-07 16:35:23 -0500

    reworked the ball and striked and added basic collision

--- src/final.py
+++ src/final.py
@@ -14,17 +14,46 @@ class Striker():
         self.color = pygame.Color(0,255,0)
         self.surface = pygame.Surface((self.radius,self.radius),
                                                 pygame.SRCALPHA)
-        self.shape = pygame.Rect(0, 0, self.radius, self.radius)
+        self._rect = pygame.Rect(0, 0, self.radius, self.radius)
     def update(self,surface):
         x,y = pygame.mouse.get_pos()
         x =  min(x,SCRNWIDTH-self.radius)
         y = max(min(y,SCRNHEIGHT-self.radius),SCRNHEIGHT//2)
-        self.pos = (x,y)
+        self._rect.update(x,y,self.radius, self.radius)
         self.draw(surface)
     def draw(self,surface):
-        pygame.draw.ellipse(self.surface, self.color, self.shape)
-        surface.blit(self.surface,self.pos)
+        pygame.draw.rect(surface, self.color, self._rect)
 
+class Ball(pygame.sprite.Sprite):
+
+    def __init__(self, pos=(0,0), radius=60):
+        print("Ball Exist")
+        self.pos = pos
+        self.radius = radius
+        self.color = pygame.Color(255,255,255)
+        self.surface = pygame.Surface((self.radius,self.radius),
+                                                pygame.SRCALPHA)
+        self._rect = pygame.Rect(0, 0, self.radius, self.radius)
+
+        self.pos_x,self.pos_y = self.pos
+        self.vel_y = 0.0
+
+    def update(self,surface,dt):
+        self.apply_gravity(dt)
+        self._rect.update(self.pos_x,self.pos_y,self.radius, self.radius)
+        self.draw(surface)
+
+    def apply_gravity(self,dt):
+        self.vel_y += (dt*0.01)
+        self.pos_y += self.vel_y   
+
+    def bounce(self,striker):
+        if self._rect.colliderect(striker._rect):
+            print("True")
+            self.vel_y *= -.9   
+
+    def draw(self,surface):
+        pygame.draw.rect(surface, self.color, self._rect)
 
         
 def main():
@@ -33,22 +62,29 @@ def main():
     pygame.display.set_caption("Breakout:Force")
     pygame.mouse.set_visible(False)
     clock= pygame.time.Clock()
-    #values
+    
+    #VALUES
     dt = 0.0
     resolution =(SCRNWIDTH,SCRNHEIGHT)
     black = pygame.Color(0,0,0)
+
+    #OBJECTS
     screen= pygame.display.set_mode(resolution)
     striker = Striker()
+    ball = Ball((SCRNWIDTH//2,0))
     running = True
     while running:
         #QUIT DETECTION
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False   
-        screen.fill(black)
+        screen.fill(black)      
         striker.update(screen)
+        ball.update(screen,dt)    
+        ball.bounce(striker)   
         pygame.display.flip()
         dt = clock.tick(60)
+        print(dt)
     pygame.quit()
 
 
